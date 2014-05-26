f3d1e2eefb31cd5276dd8e5ed2a69b544f197534
Fiona E. Campbell <fionacam@umich.edu>
2014-05-26 14:01:24 -0400

    preview chip, accounts for ties, play again button

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ $(document).ready(function(){
  	$('body').hide();
  	$('body').fadeIn({duration: 1000, easing: 'swing'});
 
+ 	$('.column').mouseover(function(){
+ 		$(this).append('<div class = "preview ' + get_color() + '"></div>');
+ 	});
+
+ 	$('.column').mouseout(function(){
+ 		$(this).children().last().remove();
+ 	});
+
  	//Get id of column clicked on, execute game play
  	$('.column').click(function(){
  		var col = parseInt($(this).attr('id'));
- 		if (valid_move){
+ 		if (grid_matrix[0][col] === ''){
+ 			max_disc--;
+ 			$(this).children().last().remove();
 	 		game_play(col);
-	 		$('#' + col).append('<div class = "' + get_color() + '"></div>');
+	 		$('#' + col).append('<div class = "bouncing ' + get_color() + '"></div>');
 	 		switch_player();
+			$(this).append('<div class = "preview ' + get_color() + '"></div>');
 	 	}
  	});
 });
 
-var valid_move = 1;
 var player = 0;
+var max_disc = 42;
 
 //2D array of chars for bookkeeping
 var grid_matrix = [
@@ -38,11 +49,8 @@ function switch_player(){
 };
 
 function get_color(){
-	if (player === 0){
-		return 'red chip';
-	} else { 
-		return 'black chip';
-	}
+	if (player === 0)	{ return 'red chip';	}
+	else 				{ return 'black chip';	}
 };
 
 //Checks for four in a row given most recently used location
@@ -50,7 +58,6 @@ function get_winner(row, col){
 
 	var count = 0;
 	var flag = 0;
-	valid_move = 1;
 
 	//Check horizontal
 	for (i = 0; i <= 6 ; i++){
@@ -116,29 +123,46 @@ function get_winner(row, col){
 	if (flag){
 		$('#grid').addClass('end', 1500);
 
-		var delay = setTimeout(function(){
-		$('#message').removeClass('hidden');
-		}, 250)
+		setTimeout(function(){
+		$('#player_win').removeClass('hidden');
+		}, 500);
 
 		if (player === 0)	{ $('#winner').append('1'); } 
 		else 				{ $('#winner').append('2'); }
+	} 
+
+	//If grid is full and nobody yet has won, declare tie & set flag
+	else if (max_disc === 0){
+
+		flag = 1;
+
+		$('#grid').addClass('end', 1500);
+
+		setTimeout(function(){
+		$('#tie').removeClass('hidden');
+		}, 500);
+	}
+
+	//If game is over, ask to play again
+	if (flag){
+		setTimeout(function(){
+		$('#button').removeClass('hidden');
+		}, 1000);
+		$('#button').click(function(){
+			location.reload();
+		});
 	}
 };
 
-//Verifies validity, marks grid, checks for winner
+//Marks grid, checks for winner
 function game_play(col){
 
 	var row;
-
-	if (grid_matrix[0][col] === ''){
-		for (var i = 5; i >= 0; i--){
-			if (grid_matrix[i][col] === ''){
-				grid_matrix[i][col] = player;
-				row = i;
-				break;
-			}
-		} 
-	} else	{ valid_move = 0; } 
-	
-	get_winner(row, col);
+	for (var i = 5; i >= 0; i--){
+		if (grid_matrix[i][col] === ''){
+			grid_matrix[i][col] = player;
+			row = i;
+			break;
+		}
+	} get_winner(row, col);
 };
